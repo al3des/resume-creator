64b59c7d2019e3532e4964428bc30249bdd9c756
al3des <alejandrodesalvo@gmail.com>
2021-02-04 22:44:31 +0100

    custom fields sidebar

--- src/components/sidebar/custom-field.js
+++ src/components/sidebar/custom-field.js
@@ -1,19 +1,23 @@
-import { makeStyles } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core"
 
 let useStyles = makeStyles({
   root: {
-    textAlign: "center"
-  }
-});
+    textAlign: "center",
+  },
+})
 
 export default function CustomField({ customField }) {
-  let classes = useStyles();
+  let classes = useStyles()
   return (
     <div className={classes.root}>
-      <h2>{customField.name}</h2>
-      {customField.text.map((value, i) => (
-        <p key={i}>{value}</p>
-      ))}
+      <h2>{customField.fieldTitle}</h2>
+      {/* {customField.value.map((value, i) => ( */}
+      <p
+      // key={i}
+      >
+        {customField.fieldValue}
+      </p>
+      {/* ))} */}
     </div>
-  );
+  )
 }
